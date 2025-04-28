dbfd1a2ab5c86f8b7f52120472a23ffcdd3222ba
Joyada Okomoto <2381038@unai.edu>
2025-04-28 12:19:49 +0700

    testing something out

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 // App.tsx
-import { Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './pages/Login';
-import RegisterPage from './pages/Register';
-import CatalogPage from './pages/MainCatalogue';
-import ProfilePage from './pages/Profile';
-import ProtectedRoute from './components/ProtectedRoute';
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+import CatalogPage from "./pages/MainCatalogue";
+import ProfilePage from "./pages/Profile";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route element={<ProtectedRoute />}>
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        </Route>
+      <Route path="/catalog" element={<CatalogPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
       <Route path="/" element={<Navigate to="/catalog" replace />} />
       <Route path="*" element={<Navigate to="/catalog" replace />} />
     </Routes>
