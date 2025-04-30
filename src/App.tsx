1152de0003417bdc9c266fb13f5d9cf387980db2
Joyada Okomoto <2381038@unai.edu>
2025-04-30 21:00:56 +0700

    you must login to enter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
-// App.tsx
-import { Routes, Route, Navigate } from "react-router-dom";
-import LoginPage from "./pages/Login";
-import RegisterPage from "./pages/Register";
-import CatalogPage from "./pages/MainCatalogue";
-import ProfilePage from "./pages/Profile";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import ProfilePage from './pages/Profile';
+import CatalogPage from './pages/MainCatalogue';
+import ProtectedRoute from './components/ProtectedRoute';
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/catalog" element={<CatalogPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/" element={<Navigate to="/catalog" replace />} />
-      <Route path="*" element={<Navigate to="/catalog" replace />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          {/* Add other protected routes here */}
+        </Route>
+        
+        {/* Redirect to login if no route matches */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
